841f5f1e3f027e7bad9ba5b75b800c43c68874dd
Skewnart <corentinz@outlook.fr>
2025-02-12 00:40:44 +0100

    tests: Adding Search tests

--- src/search.rs
+++ src/search.rs
@@ -11,17 +11,17 @@ impl Search {
         }
     }
 
-    pub fn search<'a>(self, search : &str, content : &'a str) -> Vec<&'a str> {
+    pub fn search<'a>(self, query : &str, content : &'a str) -> Vec<&'a str> {
         if self.case_insensitive {
             content
                 .lines()
-                .filter(|line| line.to_lowercase().contains(&search.to_lowercase()))
+                .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
                 .collect()
         }
         else {   
             content
                 .lines()
-                .filter(|line| line.contains(search))
+                .filter(|line| line.contains(query))
                 .collect()
         }
     }    
@@ -31,14 +31,81 @@ impl Search {
 mod tests {
     use super::*;
 
-//     #[test]
-//     fn one_result() {
-//         let query = "duct";
-//         let content = "\
-// Rust:
-// safe, fast, productive.
-// Pick three.";
+    #[test]
+    fn sensitive_no_result() {
+        let content =
+"\
+The first line.
+the second line.";
+        
+        let query = "test";
+        let search = Search { case_insensitive: false };
 
-//         assert_eq!(vec!["safe, fast, productive."], search(query, content));
-//     }
+        assert_eq!(Vec::<&str>::new(), search.search(query, content));
+    }
+
+    #[test]
+    fn sensitive_upper_one_result() {
+        let content =
+"\
+The first line.
+the second line.";
+        
+        let query = "The";
+        let search = Search { case_insensitive: false };
+
+        assert_eq!(vec!["The first line."], search.search(query, content));
+    }
+
+    #[test]
+    fn sensitive_lower_one_result() {
+        let content =
+"\
+The first line.
+the second line.";
+        
+        let query = "the";
+        let search = Search { case_insensitive: false };
+
+        assert_eq!(vec!["the second line."], search.search(query, content));
+    }
+
+    #[test]
+    fn sensitive_two_results() {
+        let content =
+"\
+The first line.
+the second line.";
+        
+        let query = "line";
+        let search = Search { case_insensitive: false };
+
+        assert_eq!(vec!["The first line.", "the second line."], search.search(query, content));
+    }
+
+    #[test]
+    fn insensitive_one_result() {
+        let content =
+"\
+The first line.
+the second line.";
+        
+        let query = "first";
+        let search = Search { case_insensitive: true };
+
+        assert_eq!(vec!["The first line."], search.search(query, content));
+    }
+
+    #[test]
+    fn insensitive_two_results() {
+        let content =
+"\
+The first line.
+the second line.";
+        
+        let query = "the";
+        let search = Search { case_insensitive: true };
+
+        assert_eq!(vec!["The first line.", "the second line."], search.search(query, content));
+    }
 }
