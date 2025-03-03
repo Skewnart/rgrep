674613f5f02ffc9391625ae4af38478c5a4732cc
Skewnart <corentinz@outlook.fr>
2025-03-03 10:38:17 +0100

    feat: Building PromptExtractor

--- lib/src/config.rs
+++ lib/src/config.rs
@@ -56,21 +56,30 @@ pub struct Parameter {
 }
 
 #[derive(Debug)]
-pub struct CmdLineExtractor {
+pub struct Prompt {
     pub program_name: String,
     pub content_piped: Option<String>,
     pub arguments: Option<Vec<String>>,
     pub parameters: Option<Vec<Parameter>>,
 }
 
-impl CmdLineExtractor {
-    pub fn build(_terminal_service : impl Terminal, mut args : impl Iterator<Item = String>) -> Result<Self, String> {
+pub struct PromptExtractor<T> where T: Terminal {
+    terminal_service : T
+}
+impl<T> PromptExtractor<T>  where T: Terminal {
+    pub fn new (_terminal_service : T) -> Self {
+        Self {
+            terminal_service : _terminal_service
+        }
+    }
+
+    pub fn extract(self, mut args : impl Iterator<Item = String>) -> Result<Prompt, String> {
 
         let Some(_program_name) = args.next() else {
             return Err("Program name was not automatically provided.".to_string());
         };
 
-        let _from_pipe = !_terminal_service.is_terminal();
+        let _from_pipe = !self.terminal_service.is_terminal();
 
         let _content_piped = if _from_pipe {
             Some(io::stdin().lock().lines().fold(String::from(""), |acc, line| acc + &line.unwrap() + "\n"))
@@ -106,7 +115,7 @@ impl CmdLineExtractor {
             _parameters.push(parameter);
         }
 
-        Ok(Self{
+        Ok(Prompt{
             program_name: _program_name,
             content_piped : _content_piped,
             arguments : if !_arguments.is_empty() { Some(_arguments) } else { None },
@@ -174,7 +183,7 @@ mod tests {
     #[test]
     fn test() {
         let args= extract_query_into_iter("program.exe query file -i");
-        let config = CmdLineExtractor::build(StdinServiceMock { is_terminal: true }, args);
+        let config = PromptExtractor::new(StdinServiceMock { is_terminal: true }).extract(args);
 
         eprintln!("{:?}", config);
     }
